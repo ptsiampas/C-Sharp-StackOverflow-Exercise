6d8c2a34af538b8059dc96ee478bf3ae48c4a2d8
Peter Tsiampas <peter@tsiampas.com>
2017-06-18 18:59:34 +1000

    Final Version

--- StackOverflow-Exercise/Program.cs
+++ StackOverflow-Exercise/Program.cs
@@ -10,6 +10,26 @@ namespace StackOverflow_Exercise
     {
         static void Main(string[] args)
         {
+            var solutionPost = new Post();
+
+            solutionPost.Title = "Adding a README.md file to a c# project in Visual Studio 2015";
+            solutionPost.Description = "Look in the top of the solution explorer for the option add hidden files. It should appear with dotted outline.\n If you wish to add it to the project you can right click and press \"add to project\"";
+            solutionPost.CreatedTime = new DateTime(2015, 12, 21);
+
+            solutionPost.VoteUp();
+            solutionPost.VoteUp();
+            solutionPost.VoteUp();
+            solutionPost.VoteUp();
+            Console.WriteLine("Should Have 4 Votes");
+
+            solutionPost.PrintFullPost();
+
+            solutionPost.VoteDown();
+            solutionPost.VoteDown();
+            Console.WriteLine("Should Have 2 Votes");
+            solutionPost.PrintFullPost();
         }
+
+
     }
 }
